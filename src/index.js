450b84226be82d939d7adbdef59af52ab61d9288
KonstantinSh <Kverde@yandex.ru>
2021-08-17 22:38:10 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@ const main = () => {
   const program = new Command();
 
   program.version('0.0.1');
-
   program.description('Compares two configuration files and shows a difference.');
+
+  program.argument('<filepath1>');
+  program.argument('<filepath2>');
+
+  program.option('-f, --format <type>', 'output format');
 
   program.parse();
 };
